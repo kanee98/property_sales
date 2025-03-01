1d971e238516d9628b318f6cf58603be32b081cd
Kanishka Udapitiya <43329812+kanee98@users.noreply.github.com>
2025-03-01 09:42:40 +0530

    Boilerplate setup

---
+++ prisma/migrations/20250301041100_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Property` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `price` DOUBLE NULL,
+    `category` VARCHAR(191) NOT NULL,
+    `images` JSON NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
